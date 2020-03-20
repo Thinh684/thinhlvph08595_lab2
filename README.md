f46b9eff71197976b4b10f09477d251c21a25356
Thinh684 <thinhlv684@gmail.com>
2020-03-20 16:33:35 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# thinhlv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/thinhlv)
---
+++ src/app/MockData.ts
@@ -0,0 +1,86 @@
+
+import {Product} from './Product'
+export const data: Product[] = [
+    {
+        id: 1,
+        name: "Hàng 1",
+        price: 24.99,
+        desc: 'Hàng việt nam chất lượng cao',
+        status: false,
+        img: 'https://znews-photo.zadn.vn/w660/Uploaded/mdf_drkydd/2019_08_10/0.jpg'
+      },
+      {
+        id: 2,
+        name: "Hàng 2",
+        price: 24.99,
+        desc: 'Hàng việt nam chất lượng cao',
+        status: false,
+        img: 'https://znews-photo.zadn.vn/w660/Uploaded/mdf_drkydd/2019_08_10/0.jpg'
+      },
+       {
+        id: 3,
+        name: "Hàng 3",
+        price: 24.99,
+        desc: 'Hàng việt nam chất lượng cao',
+        status: false,
+        img: 'https://znews-photo.zadn.vn/w660/Uploaded/mdf_drkydd/2019_08_10/0.jpg'
+      },
+        {
+        id: 4,
+        name: "Hàng 4",
+        price: 24.99,
+        desc: 'Hàng việt nam chất lượng cao',
+        status: false,
+        img: 'https://cdn.pose.com.vn/assets/2019/03/07/5301629225053921961409043040056750658027520n-1551924968498331276673.jpg'
+      },
+        {
+        id: 5,
+        name: "Hàng 5",
+        price: 24.99,
+        desc: 'Hàng việt nam chất lượng cao',
+        status: false,
+        img: 'https://cdn.pose.com.vn/assets/2019/03/07/5301629225053921961409043040056750658027520n-1551924968498331276673.jpg'
+      },
+       {
+        id: 6,
+        name: "Hàng 6",
+        price: 24.99,
+        desc: 'Hàng việt nam chất lượng cao',
+        status: false,
+        img: 'https://cdn.pose.com.vn/assets/2019/03/07/5301629225053921961409043040056750658027520n-1551924968498331276673.jpg'
+      },  {
+        id: 7,
+        name: "Hàng 7",
+        price: 24.99,
+        desc: 'Hàng việt nam chất lượng cao',
+        status: false,
+        img: 'https://znews-photo.zadn.vn/w660/Uploaded/mdf_drkydd/2019_08_10/0.jpg'
+      }
+      ,  {
+        id: 8,
+        name: "Hàng 8",
+        price: 24.99,
+        desc: 'Hàng việt nam chất lượng cao',
+        status: false,
+        img: 'https://znews-photo.zadn.vn/w660/Uploaded/mdf_drkydd/2019_08_10/0.jpg'
+      }
+      ,  {
+        id: 9,
+        name: "Hàng 9",
+        price: 24.99,
+        desc: 'Hàng việt nam chất lượng cao',
+        status: false,
+        img: 'https://znews-photo.zadn.vn/w660/Uploaded/mdf_drkydd/2019_08_10/0.jpg'
+      },
+       
+         {
+        id: 10,
+        name: "Hàng 10",
+        price: 24.99,
+        desc: 'Hàng việt nam chất lượng cao',
+        status: false,
+        img: 'https://cdn.pose.com.vn/assets/2019/03/07/5301629225053921961409043040056750658027520n-1551924968498331276673.jpg'
+      }
+
+      ]
+
